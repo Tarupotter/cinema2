57429f9998bb7cc0d0cb0fd28202f426cf151496
El-Duckster <144563447+El-Duckster@users.noreply.github.com>
2024-12-22 21:09:43 +0100

    create sections, title and paragraph for each key in aboutUs.json

--- src/js/aboutUs.js
+++ src/js/aboutUs.js
@@ -1,17 +1,56 @@
 import {
   currentLanguage,
-  toggleLanguage,
+toggleLanguage,
   fetchData,
 } from './helpers/languageManager';
 
-const aboutUsContainer= document.querySelector('#about-us-container')
-const languageTogglerBtn = document.createElement('button');
-
-const loadAboutUsItems = async (language) =>
+const loadAboutUsPage = () =>
 {
-    const data = await fetchData( '/database/aboutUs.json' )
-     const { introduction, origin, vision } = data.aboutUs[language];
-    console.log(introduction, origin, vision)
+ const aboutUsContainer = document.querySelector('#about-us-container');
+  const languageTogglerBtn = document.createElement( 'button' );
+    console.log(aboutUsContainer)
+
+  const loadAboutUsItems = async (language) => {
+      const data = await fetchData( '/database/aboutUs.json' );
+      console.log( data );
+
+        if (!data || !data.aboutUs || !data.aboutUs[language]) {
+          console.error('Invalid data or language not found');
+          return;
+        }
+
+      const aboutUsItems = {
+          introduction: data.aboutUs[ language ].introduction,
+          origin: data.aboutUs[ language ].origin,
+          vision: data.aboutUs[ language ].vision
+      }
+
+      Object.entries(aboutUsItems).forEach(([key, value]) => {
+        const section = document.createElement('section');
+        section.className = `about-us__${key}`;
+
+        const title = document.createElement('h2');
+         title.className = `about-us__${key}--title`;
+        title.textContent = key.charAt(0).toUpperCase() + key.slice(1);
+
+        const paragraph = document.createElement('p');
+        paragraph.className = `about-us__${key}--text`;
+        paragraph.textContent = value;
+
+        section.appendChild(title);
+        section.appendChild(paragraph);
+
+        aboutUsContainer.appendChild(section);
+      });
+
+  };
+
+ loadAboutUsItems('sv');
 }
 
-loadAboutUsItems('sv')
+
+document.addEventListener( 'DOMContentLoaded', () =>
+{
+    console.log('DOM fully loaded and parsed');
+  loadAboutUsPage();
+} );
