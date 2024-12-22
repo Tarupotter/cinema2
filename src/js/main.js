962b602e950de29de4d8327ec8880d856b314d64
nilsishome <nilsnyberg24@gmail.com>
2024-12-22 21:00:11 +0100

    Initial commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,9 @@
-import '../styles/main.scss';
+import "../styles/main.scss";
+
+// Section for upcoming movies on index.html (start page only)
+function upcomingMovies() {
+  const section = document.querySelector(".upcoming-movies");
+  const movieContainer = document.createElement("div");
+  const movieImage = document.createElement("img");
+  const movieTitle = document.createElement("");
+}
