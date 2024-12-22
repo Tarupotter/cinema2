032a55ff40b8aa43c8b0ce3171f8da60cae11c4d
El-Duckster <144563447+El-Duckster@users.noreply.github.com>
2024-12-22 22:28:39 +0100

    Style about-us text

--- src/js/aboutUs.js
+++ src/js/aboutUs.js
@@ -1,56 +1,54 @@
 import {
   currentLanguage,
-toggleLanguage,
+  toggleLanguage,
   fetchData,
 } from './helpers/languageManager';
 
-const loadAboutUsPage = () =>
-{
- const aboutUsContainer = document.querySelector('#about-us-container');
-  const languageTogglerBtn = document.createElement( 'button' );
-    console.log(aboutUsContainer)
+import '../styles/about.scss';
+import '../styles/main.scss';
+const loadAboutUsPage = () => {
+  const aboutUsContainer = document.querySelector('#about-us-container');
+  const languageTogglerBtn = document.createElement('button');
+  console.log(aboutUsContainer);
 
   const loadAboutUsItems = async (language) => {
-      const data = await fetchData( '/database/aboutUs.json' );
-      console.log( data );
+    const data = await fetchData('/database/aboutUs.json');
+    console.log(data);
 
-        if (!data || !data.aboutUs || !data.aboutUs[language]) {
-          console.error('Invalid data or language not found');
-          return;
-        }
+    if (!data || !data.aboutUs || !data.aboutUs[language]) {
+      console.error('Invalid data or language not found');
+      return;
+    }
 
-      const aboutUsItems = {
-          introduction: data.aboutUs[ language ].introduction,
-          origin: data.aboutUs[ language ].origin,
-          vision: data.aboutUs[ language ].vision
-      }
+    const aboutUsItems = {
+      introduction: data.aboutUs[language].introduction,
+      origin: data.aboutUs[language].origin,
+      vision: data.aboutUs[language].vision,
+    };
 
-      Object.entries(aboutUsItems).forEach(([key, value]) => {
-        const section = document.createElement('section');
-        section.className = `about-us__${key}`;
+    Object.entries(aboutUsItems).forEach(([key, value]) => {
+      const section = document.createElement('section');
+      section.className = `about-us__section about-us__section__${key}`;
 
-        const title = document.createElement('h2');
-         title.className = `about-us__${key}--title`;
-        title.textContent = key.charAt(0).toUpperCase() + key.slice(1);
+      const title = document.createElement('h2');
+      title.className = `about-us__section__${key}--title`;
+      title.textContent = key.charAt(0).toUpperCase() + key.slice(1);
 
-        const paragraph = document.createElement('p');
-        paragraph.className = `about-us__${key}--text`;
-        paragraph.textContent = value;
+      const paragraph = document.createElement('p');
+      paragraph.className = `about-us__section__${key}--text`;
+      paragraph.textContent = value;
 
-        section.appendChild(title);
-        section.appendChild(paragraph);
+      section.appendChild(title);
+      section.appendChild(paragraph);
 
-        aboutUsContainer.appendChild(section);
-      });
-
+      aboutUsContainer.appendChild(section);
+    });
   };
 
- loadAboutUsItems('sv');
-}
+  loadAboutUsItems('sv');
+};
 
-
-document.addEventListener( 'DOMContentLoaded', () =>
-{
-    console.log('DOM fully loaded and parsed');
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('DOM fully loaded and parsed');
   loadAboutUsPage();
-} );
+});
