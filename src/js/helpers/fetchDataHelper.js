138a73fcf1e3ed7428431ac257a966552d4299b3
El-Duckster <144563447+El-Duckster@users.noreply.github.com>
2024-12-27 11:39:38 +0100

    return  a fallback value 'null' in case of an error

--- src/js/helpers/fetchDataHelper.js
+++ src/js/helpers/fetchDataHelper.js
@@ -5,13 +5,16 @@ export const fetchData = async ( filePath ) =>
         const res = await fetch( filePath );
         if ( !res.ok )
         {
-            throw new Error( `HTTP error! status:${res.status}` );   
+            throw new Error(
+              `Failed to fetch data from ${filePath}. HTTP status: ${res.status}`,
+            );
         }
         const data = await res.json();
         return data
         
     } catch ( error ){
-       console.error('error in fetching from aboutUs.json', error) 
+       console.error(`Error fetching data from ${filePath}:`, error);
+      return null;
         
     }
 }
