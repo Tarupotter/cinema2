fb8e621dd2cdc27806fca53a5c75278c62b5a6d3
El-Duckster <144563447+El-Duckster@users.noreply.github.com>
2024-12-29 23:51:08 +0100

    fetch and add dynamiccafe  menu content to DOM

---
+++ src/js/cafe.js
@@ -0,0 +1,100 @@
+import {fetchData } from './helpers/fetchDataHelper.js';
+import {
+  currentLanguage,
+  createLanguageToggleBtn,
+} from './components/languageToggleBtn.js';
+
+
+import '../styles/cafe.scss';
+
+const menuContainer = document.querySelector( '#cafe-menu-container');;
+
+
+const loadMenuItems = async ( menu, language ) =>
+{
+    menuContainer.innerHTML = '';
+
+    const snacksSection = document.createElement('section');
+    snacksSection.className = 'menu-section snacks-section';
+    
+    const snacksTitle = document.createElement( 'h2' );
+    snacksTitle.className = 'menu-section__title menu-section__title-snacks';
+    snacksTitle.textContent = language === 'sv' ? 'Snacks' : 'Snacks';
+    snacksSection.appendChild(snacksTitle);
+    
+    
+    
+    const snacksList = document.createElement( 'ul' );
+    snacksList.className = 'cafe-menu__list cafe-menu__List__snacks'
+    
+    menu.snacks.forEach((item) => {
+        const menuItem = document.createElement('li');
+        menuItem.className = 'cafe-menu__item cafe-menu__List__snacks-item';
+        menuItem.textContent = `${item.name[language]}`;
+        
+        const menuItemPrice = document.createElement( 'span' );
+        menuItemPrice.textContent = `${ item.price } SEK`;
+        
+        menuItem.appendChild( menuItemPrice );
+        
+        snacksList.appendChild( menuItem );
+        console.log(snacksList)
+    } );
+    
+    const beveragesSection = document.createElement('section');
+    beveragesSection.className = 'menu-section beverages-section';
+
+    const beveragesTitle = document.createElement('h2');
+    beveragesTitle.className ='menu-section__title menu-section__title-beverages';
+    beveragesTitle.textContent = language === 'sv' ? 'Drycker' : 'Beverages';
+    beveragesSection.appendChild( beveragesTitle );
+    
+     const beveragesList = document.createElement('ul');
+     beveragesList.className = 'cafe-menu__list cafe-menu__List__beverages';
+
+      menu.beverages.forEach((item) => {
+        const menuItem = document.createElement('li');
+        menuItem.className = 'cafe-menu__item cafe-menu__List__bevrages-item';
+        menuItem.textContent = `${ item.name[ language ] } `;
+          
+        const menuItemPrice = document.createElement( 'span' );
+        menuItemPrice.textContent = `${ item.price } SEK`;
+
+        menuItem.appendChild(menuItemPrice);
+        beveragesList.appendChild(menuItem);
+      } );
+    
+      snacksSection.appendChild(snacksList);
+    beveragesSection.appendChild(beveragesList);
+    
+      menuContainer.appendChild(snacksSection);
+      menuContainer.appendChild(beveragesSection);
+};
+    const loadCafeMenu = async (language) => {
+    const data = await fetchData('/database/cafeMenu.json');
+    console.log( data );
+    
+  if (data && data.menu) {
+    loadMenuItems(data.menu, language);
+  } else {
+      console.error( 'Failed to load cafe menu data.' );
+      return [];
+  }
+};
+document.addEventListener('DOMContentLoaded', () => {
+  const cafeMenuContainer = document.createElement('div');
+  cafeMenuContainer.id = 'cafe-menu-container';
+  document.body.appendChild(cafeMenuContainer);
+
+  loadCafeMenu(currentLanguage);
+
+  const languageToggleBtn = createLanguageToggleBtn(
+    currentLanguage,
+    (newLanguage) => {
+      loadCafeMenu(newLanguage);
+    },
+  );
+
+  document.body.prepend(languageToggleBtn);
+});
+
