ee52b737b8a8b6f98603ac9965f472957d78b788
Taru Keskinen <taru.keskinen@lerniastudent.se>
2025-01-15 22:24:31 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -13,5 +13,6 @@ app.get('/', async (req, res) =>{
 app.use('/Group-d-assignment', express.static('../dist/assets')); 
 app.use('/assets', express.static('../dist/assets'));
 app.use('/images', express.static('../dist/images'));
+app.use('/data', express.static('../dist/dataBase'));
 
 app.listen(3080);
