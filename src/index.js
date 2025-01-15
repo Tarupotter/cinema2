f606e57e293c826167b35c6d4ff150a3044774cd
Taru Keskinen <taru.keskinen@lerniastudent.se>
2025-01-15 21:25:59 +0100

    resolved index.html path

--- src/index.js
+++ src/index.js
@@ -4,21 +4,12 @@ import fs from 'fs/promises';
 const app = express();
 
 app.get('/', async (req, res) =>{
-    const buf = await fs.readFile('./static/index.html');
+    const buf = await fs.readFile('../index.html');
     const html = buf.toString();
     res.send(html);
 });
 
-app.get('/', async (req, res) =>{
-    const buf = await fs.readFile('./static/index.html');
-    const html = buf.toString();
-    const name = req.params.name;
 
-    const changedHtml = html.replace('Retro', name);
-    res.send(changedHtmltml);
-});
-
-
-/*app.use('', express.static(''));  Här behöver jag hjälp */
+app.use('/Group-d-assignment', express.static('./dist/assets'));  
 
 app.listen(3080);
