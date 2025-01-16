174ad1f800d2bf5e75f17b42473e85fa1a950cfd
Taru Keskinen <taru.keskinen@lerniastudent.se>
2025-01-16 17:59:04 +0100

    all link paths ok

--- server.js
+++ server.js
@@ -9,6 +9,30 @@ app.get('/', async (req, res) =>{
     res.send(html);
 });
 
+app.get('/index.html', async (req, res) =>{
+    const buf = await fs.readFile('./dist/index.html');
+    const html = buf.toString();
+    res.send(html);
+});
+
+app.get('/about.html', async (req, res) =>{
+    const buf = await fs.readFile('./dist/about.html');
+    const html = buf.toString();
+    res.send(html);
+});
+
+app.get('/contact.html', async (req, res) =>{
+    const buf = await fs.readFile('./dist/contact.html');
+    const html = buf.toString();
+    res.send(html);
+});
+
+app.get('/cafe.html', async (req, res) =>{
+    const buf = await fs.readFile('./dist/cafe.html');
+    const html = buf.toString();
+    res.send(html);
+});
+
 app.use('/Group-d-assignment', express.static('./dist/assets')); 
 app.use('/assets', express.static('./dist/assets'));
 app.use('/images', express.static('./dist/images'));
