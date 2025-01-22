1a3d68abf4a5223a46b8eda15588feac2a19f764
Taru Keskinen <taru.keskinen@lerniastudent.se>
2025-01-22 13:01:50 +0100

    organize handlebars

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import fs from 'fs/promises';
+/*import fs from 'fs/promises';   Behöver jag denna längre?!? */
 import { engine } from 'express-handlebars';
 
 const app = express();
@@ -7,37 +7,44 @@ app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
-async function renderPage(res, page) {
-    console.log(`Rendering page: ${page}`); /* kom ihåg att ändra sökvägen i json */
-    res.render(page);
+async function renderPage(res, page, theTitle) {
+    console.log(`Rendering page: ${page}`); 
+
+    res.render(page, {title: theTitle});
 }
 
 app.get('/', (req, res) =>{
-   renderPage(res, 'index');
+   renderPage(res, 'index', 'Retro');
 });
 
 app.get('/index', (req, res) =>{
-    renderPage(res, 'index');
+    renderPage(res, 'index', 'Retro');
 });
 
-app.get('/about', (req, res) =>{
-    renderPage(res, 'about');
-});
-
-app.get('/contact', (req, res) =>{
-    renderPage(res, 'contact');
+app.get('/movies', (req, res) =>{
+    renderPage(res, 'movies', 'Filmer');
 });
 
 app.get('/cafe', (req, res) =>{
-    renderPage(res, 'cafe');
+    renderPage(res, 'cafe', 'Servering');
 });
 
-app.use('/Group-d-assignment', express.static('../dist/assets')); 
+app.get('/about', (req, res) =>{
+    renderPage(res, 'about', 'Om oss');
+});
+
+app.get('/contact', (req, res) =>{
+    renderPage(res, 'contact', 'Kontakta oss');
+});
+
+
+
+/*app.use('/Group-d-assignment', express.static('../dist/assets')); */
 app.use('/assets', express.static('../dist/assets'));
 app.use('/images', express.static('../dist/images'));
 app.use('/database', express.static('../dist/database'));
 
 app.listen(5080, () => {
-console.log('App is running on port 5080');
+console.log('running on port 5080');
 
 }); 
