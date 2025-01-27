5e76fffc87ea3c5b92ecf7d7b76286d8e4757d70
Taru Keskinen <taru.keskinen@lerniastudent.se>
2025-01-27 18:50:16 +0100

    remove dist from gitignore

---
+++ dist/assets/languageToggleBtn-CJhW3R8M.js
@@ -0,0 +1,2 @@
+let n="sv";const s=e=>{n=n==="sv"?"en":"sv",e.textContent=n==="sv"?"In English":"Till Svenska"},g=(e,a)=>{const t=document.createElement("button");return t.id="language-toggle-btn",t.textContent=e==="sv"?"In English":"Till Svenska",t.addEventListener("click",()=>{s(t),a&&typeof a=="function"&&a(n)}),t};export{n as a,g as c};
+//# sourceMappingURL=languageToggleBtn-CJhW3R8M.js.map
